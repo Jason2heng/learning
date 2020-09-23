bf408761510c86c72237e5119aa08ab824333522
你的GitHub用户名 <GitHub>
2020-09-23 23:11:17 +0800

    测速

---
+++ src/main/java/com/doaredo/mvc/DoaredoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.doaredo.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoaredoMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoaredoMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doaredo/mvc/DoaredoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doaredo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoaredoMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
