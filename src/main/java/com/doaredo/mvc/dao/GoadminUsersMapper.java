54ce5174e79a7191dd6ae7f57e3d3f4894fa462a
郑伟钊 <327569753@qq.com>
2020-09-27 11:27:09 +0800

    业务框架基础封装-增删改查

---
+++ src/main/java/com/doaredo/mvc/dao/GoadminUsersMapper.java
@@ -0,0 +1,7 @@
+package com.doaredo.mvc.dao;
+
+import com.doaredo.mvc.core.Mapper;
+import com.doaredo.mvc.model.GoadminUsers;
+@org.apache.ibatis.annotations.Mapper
+public interface GoadminUsersMapper extends Mapper<GoadminUsers> {
+}
