5e742f5aaff26aebf5b617c72309b9481ebe7a1d
郑伟钊 <327569753@qq.com>
2020-09-27 14:02:08 +0800

    恢复master代码

--- src/main/java/com/doaredo/mvc/dao/GoadminUsersMapper.java
+++
@@ -1,7 +0,0 @@
-package com.doaredo.mvc.dao;
-
-import com.doaredo.mvc.core.Mapper;
-import com.doaredo.mvc.model.GoadminUsers;
-@org.apache.ibatis.annotations.Mapper
-public interface GoadminUsersMapper extends Mapper<GoadminUsers> {
-}
